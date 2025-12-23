6be4d3655b419753321ceee4ee4fba1d3f3d8ecb
alexraspopin <raspopin.aleksandr@otr.ru>
2025-12-23 14:19:39 +0300

    init

---
+++ src/main/java/com/otr/portal/gateway/EtdGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.otr.portal.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtdGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtdGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otr/portal/gateway/EtdGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otr.portal.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtdGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
